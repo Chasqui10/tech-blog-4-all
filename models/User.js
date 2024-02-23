e385e520e94b2b7a2c9a30d68476570cbcecb85c
Chasqui10 <chasqui.rmf@gmail.com>
2024-02-23 14:09:43 -0700

    Initial still needs more work

---
+++ models/User.js
@@ -0,0 +1,58 @@
+const {} = require('sequelize');
+const bycrypt = require('bcrypt');
+const sequelize = require('../config/connection');
+
+class User extends Model {
+    checkPassword(loginPw) {
+        return bycrypt.compareSync(loginPw, this.password);
+    }
+}
+
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+          len: [8]
+      }
+    }
+  },
+  {
+    hooks: {
+        async beforeCreate(newUserData) {
+            newUserData.password = await bycrypt.hash(newUserData.password, 10);
+            return newUserData;
+        },
+        async beforeUpdate(updatedUserData) {
+            updatedUserData.password = await bycrypt.hash(updatedUserData.password, 10);
+            return updatedUserData;
+        }
+    },
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'user'
+  }
+);
+
+module.exports = User;
